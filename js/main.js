cc78b269d2d0ec25555a85a323cf990b0f97b05d
Alexis Landa <114886294+Alexis2428@users.noreply.github.com>
2022-11-13 19:23:17 -0300

    Agrega validaciones
    Agrega las validaciones correspondientes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,42 @@
 const $formulario = document.formulario;
+
+const nombre = $formulario.nombre.value;
+const ciudad = $formulario.ciudad.value;
+const comportamiento = $formulario.comportamiento.value;
+const descripcionRegalo = $formulario['descripcion-regalo'].value;
+
+// console.log(ciudad);
+// console.log(comportamiento);
+// console.log(descripcionRegalo);
+
+function validarNombre (nombre) {
+    if (0 === nombre.length) {
+        return 'Este campo debe tener al menos 1 caracter';
+    }
+
+    if (50 <= nombre.length) {
+        return 'Este campo debe tener menos de 50 caracteres';
+    }
+
+    return '';
+}
+
+function validarCiudad(ciudad) {
+    if ('' === ciudad) {
+        return 'Debe seleccionar una opción';
+    }
+
+    return '';
+}
+
+function validarDescripcionRegalo(descripcionRegalo) {
+    if (0 === descripcionRegalo.length) {
+        return 'Este campo debe tener al menos 1 caracter';
+    }
+
+    if (50 <= descripcionRegalo.length) {
+        return 'Este campo debe tener menos de 50 caracteres';
+    }
+
+    return '';
+}
