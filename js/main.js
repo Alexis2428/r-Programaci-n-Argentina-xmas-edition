bab54eac544166a4242b86514658a1ca5257a6de
Alexis Landa <114886294+Alexis2428@users.noreply.github.com>
2022-12-01 16:32:55 -0300

    Agrega función para usar las validaciones
    Valida los campos del formulario

--- js/main.js
+++ js/main.js
@@ -38,5 +38,91 @@ function validarDescripcionRegalo(descripcionRegalo) {
     return '';
 }
 
+function validarFormulario(event) {
+    const $form = document.formulario;
+
+    const nombre = $form.nombre.value;
+    const ciudad = $form.ciudad.value;
+    const descripcionRegalo = $form['descripcion-regalo'].value;
+
+    const errores = {
+        nombre: validarNombre(nombre),
+        ciudad: validarCiudad(ciudad),
+        'descripcion-regalo': validarDescripcionRegalo(descripcionRegalo)
+    };
+
+    const esExito = manejarErrores(errores) === 0;
+
+    if (esExito) {
+        $form.className = 'oculto';
+        document.querySelector('#exito').className = '';
+
+        setTimeout(function(){window.location.href = 'wishlist.html'}, 5000);
+    }
+
+    event.preventDefault();
+}
+
+function manejarErrores(errores) {
+    const $listaErrores = document.querySelectorAll('#errores li');
+    let cantidadErrores = 0;
+    
+    Object.keys(errores).forEach(function(llave){
+        const error = errores[llave];
+
+        if (error) {
+            cantidadErrores++;
+            if (!comprobarExisteError(error, $listaErrores)) {
+
+                $form[llave].className = 'error';
+                crearError(error);
+
+            }
+        } else {
+            $form[llave].className = '';
+        }
+    });
+
+    borrarErroresCorregidos(errores, $listaErrores);
+
+    return cantidadErrores;
+}
+
+function crearError(error) {
+    const $errores = document.querySelector('#errores');
+
+    const $error = document.createElement('li');
+    $error.innerText = error;
+    $errores.appendChild($error);
+}
+
+function borrarErroresCorregidos(errores, $listaErrores) {
+    const valorErrores = Object.values(errores);
+
+    for (let i=0; i < $listaErrores.length; i++) {
+        let existeEnErrores = false;
+
+        for (let j=0; j < valorErrores.length; j++) {
+            if ($listaErrores[i].innerText === valorErrores[j]) {
+                existeEnErrores = true;
+                break;
+            }
+        }
+
+        if (!existeEnErrores) {
+            $listaErrores[i].remove();
+        }
+    }
+}
+
+function comprobarExisteError(error, errores) {
+    for (let i=0; i < errores.length; i++) {
+        if (error === errores[i].innerText) {
+            return true;
+        }
+    }
+    return false;
+}
+
 const $form = document.formulario;
 $form.onsubmit = validarFormulario;
